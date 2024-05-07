7015ee77ad88212a9e18fd04dfcbcf90a75618c6
Foad <fwad40901@gmail.com>
2024-05-08 00:20:34 +0330

    Show data in details com

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ function App() {
             <Route exact path="/">
               <Home />
             </Route>
-            <Route path="/create">
+            <Route exact path="/create">
               <Create />
             </Route>
-            <Route path="/blogs/:id">
+            <Route exact path="/blogs/:id">
               <BlogDetails />
             </Route>
           </Switch>
--- src/components/Blogs/BlogDetails.js
+++ src/components/Blogs/BlogDetails.js
@@ -1,11 +1,24 @@
 import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
+import useFetch from '../../hooks/useFetch';
 
 const BlogDetails = () => {
   const { id: idParam } = useParams();
-  // console.log(idParam);
+  const {
+    data: blog,
+    isPending,
+    error,
+  } = useFetch(`http://localhost:8000/blogs/${idParam}`);
   return (
     <div className="blog-details">
-      <h2>Blog details - {idParam}</h2>
+      {isPending && <h2>Loading...</h2>}
+      {error && <h2>{error}</h2>}
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <p>Write by {blog.author}</p>
+          <div>{blog.body}</div>
+        </article>
+      )}
     </div>
   );
 };
