69d15f09ddce6c5624f173500593ea36a85da998
Foad <fwad40901@gmail.com>
2024-05-08 00:37:27 +0330

    Create Form for add blog

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,7 +1,33 @@
+import { useState } from 'react';
+
 const Create = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('');
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form>
+        <label>Blog Title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+        />
+        <label>Blog Body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={e => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog Author:</label>
+        <select value={author} onChange={e => setAuthor(e.target.value)}>
+          <option value="miad">miad</option>
+          <option value="foad">foad</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
     </div>
   );
 };
